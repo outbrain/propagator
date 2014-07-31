95248c5fb96c432196e12a2b6b6e87c5c0983c2e
snoach <snoach@outbrain.com>
2014-07-31 09:25:54 +0300

    Checking whether schema is required on input_script, and disabling schema input when not required.

--- install-db-migration-2014-07-30.sql
+++ install-db-migration-2014-07-30.sql
@@ -1,2 +1,17 @@
 alter table propagate_script_instance_deployment 
-add column manual_approved tinyint unsigned default 0;
+    add column manual_approved tinyint unsigned default 0;
+
+alter table database_role add column has_schema tinyint unsigned default 1;
+
+
+CREATE TABLE `database_role_known_deploy_schema` (
+  `database_role_id` varchar(32) CHARACTER SET ascii NOT NULL,
+  `known_deploy_schema_id` int(10) unsigned NOT NULL,
+  `is_default` tinyint(3) unsigned DEFAULT '0',
+  PRIMARY KEY (`database_role_id`,`known_deploy_schema_id`),
+  KEY `schema_idx` (`known_deploy_schema_id`),
+  CONSTRAINT `role_schema_known_deploy_schema_id_fk` FOREIGN KEY (`known_deploy_schema_id`) REFERENCES `known_deploy_schema` (`known_deploy_schema_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `role_schema_database_role_id_fk` FOREIGN KEY (`database_role_id`) REFERENCES `database_role` (`database_role_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+;
+
