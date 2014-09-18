52f3a9cf7f57e3288d787266954eef4305c3849a
snoach <snoach@outbrain.com>
2014-09-18 12:09:04 +0300

    a bit of a tag mess, retagging as 1.0.3

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Requires fpm: https://github.com/jordansissel/fpm
 #
 
-release_version="1.0.2"
+release_version="1.0.3"
 release_dir=/tmp/propagator-release
 release_files_dir=$release_dir/propagator
 rm -rf $release_dir/*
@@ -15,7 +15,9 @@ cd  $(dirname $0)
 rsync -av --delete --delete-excluded --exclude=".svn" --exclude=".git" ./ $release_files_dir/
 
 cd $release_dir
+tar cfz propagator-"${release_version}".tar.gz ./propagator
 fpm -v "${release_version}" -f -s dir -t rpm -n propagator -C $release_files_dir --prefix=/var/www/html/propagator .
+fpm -v "${release_version}" -f -s dir -t deb -n propagator -C $release_files_dir --prefix=/var/www/html/propagator .
 
 echo "---"
 echo "Done. Find releases in $release_dir"
